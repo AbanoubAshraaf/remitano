409f9ad2871dafc4691ca1b83bc0199e23692678
AbanoubAshraaf <abanoubashrf25@gmail.com>
2019-12-24 22:35:28 +0200

    fix VideoitemView UI

--- App/Components/VideoItemView/VideoItemView.js
+++ App/Components/VideoItemView/VideoItemView.js
@@ -88,59 +88,63 @@ class VideoItemView extends Component {
     this.setState({currentTime});
   };
 
+  detailsView = fullScreen => {
+    return (
+      <View style={{marginLeft: 10, flex: 1, marginTop: fullScreen ? 10 : 0}}>
+        <Text style={styles.title}>{this.props.item.title}</Text>
+        <Text style={styles.sharedby}>{this.props.item.sharedby}</Text>
+        <ScrollView style={{flex: 1, height: 100}}>
+          <Text style={styles.description}>{this.props.item.description}</Text>
+        </ScrollView>
+      </View>
+    );
+  };
   render() {
     return (
-      <View style={styles.container}>
-        <View
-          style={[
-            styles.VideoContainer,
-            {
-              height: this.state.height,
-              width: this.state.width,
-            },
-          ]}>
-          <Video
-            onEnd={this.onEnd}
-            onLoad={this.onLoad}
-            onLoadStart={this.onLoadStart}
-            onProgress={this.onProgress}
-            paused={this.state.paused}
-            ref={videoPlayer => (this.videoPlayer = videoPlayer)}
-            resizeMode={this.state.screenType}
-            onFullScreen={this.state.isFullScreen}
-            source={{
-              uri:
-                'https://r5---sn-4g5ednsz.googlevideo.com/videoplayback?expire=1577233683&ei=s1gCXp7XM5nlkgaCoKLQDA&ip=2604%3A180%3A3%3A376%3A1041%3Ab55%3Adb8f%3A33bd&id=o-AM-yeTZhja_M1uqgSQmNEtuTc8y0IB0gUDsL4Iq0Xc-J&itag=18&source=youtube&requiressl=yes&mime=video%2Fmp4&gir=yes&clen=5301951&ratebypass=yes&dur=66.501&lmt=1558113611533227&fvip=5&fexp=23842630&c=WEB&txp=5531432&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cmime%2Cgir%2Cclen%2Cratebypass%2Cdur%2Clmt&sig=ALgxI2wwRgIhAI0tSn_FrYZzUgOUH1ajeiJKUCx8U831sk9phQR-RPhUAiEAoA9Qt8Of5Ie1vjqLvmfBgQdQVDntqTQMLo68IGbD5oI%3D&redirect_counter=1&cm2rm=sn-n4ve676&req_id=d3f401a0f8f6a3ee&cms_redirect=yes&mip=41.44.48.176&mm=34&mn=sn-4g5ednsz&ms=ltu&mt=1577212049&mv=m&mvi=4&pl=19&lsparams=mip,mm,mn,ms,mv,mvi,pl&lsig=AHylml4wRQIgOXQz80MGz5Ok8e_wFjZ_YsRl49Mzl2vSPdBLyUdhIrgCIQCOa6EpKdzW8eYZzEgGChqs-un4POPsfqw6Y1gdEHotgg==',
-            }}
-            style={styles.mediaPlayer}
-            volume={this.state.rendered ? 1 : 0}
-          />
-          <MediaControls
-            duration={this.state.duration}
-            isLoading={this.state.isLoading}
-            mainColor="#333"
-            onFullScreen={this.onFullScreen}
-            onPaused={this.onPaused}
-            onReplay={this.onReplay}
-            onSeek={this.onSeek}
-            onSeeking={this.onSeeking}
-            playerState={this.state.playerState}
-            progress={this.state.currentTime}
-            toolbar={this.renderToolbar()}
-          />
-        </View>
-        {this.state.width == 150 && (
-          <View style={{marginLeft: 10}}>
-            <Text style={styles.title}>{this.props.item.title}</Text>
-            <Text style={styles.sharedby}>{this.props.item.sharedby}</Text>
-            <ScrollView style={{flex: 1, height: 100}}>
-              <Text style={styles.description}>
-                {this.props.item.description}
-              </Text>
-            </ScrollView>
+      <>
+        <View style={styles.container}>
+          <View
+            style={[
+              styles.VideoContainer,
+              {
+                height: this.state.height,
+                width: this.state.width,
+              },
+            ]}>
+            <Video
+              onEnd={this.onEnd}
+              onLoad={this.onLoad}
+              onLoadStart={this.onLoadStart}
+              onProgress={this.onProgress}
+              paused={this.state.paused}
+              ref={videoPlayer => (this.videoPlayer = videoPlayer)}
+              resizeMode={this.state.screenType}
+              onFullScreen={this.state.isFullScreen}
+              source={{
+                uri:
+                  'https://r5---sn-4g5ednsz.googlevideo.com/videoplayback?expire=1577233683&ei=s1gCXp7XM5nlkgaCoKLQDA&ip=2604%3A180%3A3%3A376%3A1041%3Ab55%3Adb8f%3A33bd&id=o-AM-yeTZhja_M1uqgSQmNEtuTc8y0IB0gUDsL4Iq0Xc-J&itag=18&source=youtube&requiressl=yes&mime=video%2Fmp4&gir=yes&clen=5301951&ratebypass=yes&dur=66.501&lmt=1558113611533227&fvip=5&fexp=23842630&c=WEB&txp=5531432&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cmime%2Cgir%2Cclen%2Cratebypass%2Cdur%2Clmt&sig=ALgxI2wwRgIhAI0tSn_FrYZzUgOUH1ajeiJKUCx8U831sk9phQR-RPhUAiEAoA9Qt8Of5Ie1vjqLvmfBgQdQVDntqTQMLo68IGbD5oI%3D&redirect_counter=1&cm2rm=sn-n4ve676&req_id=d3f401a0f8f6a3ee&cms_redirect=yes&mip=41.44.48.176&mm=34&mn=sn-4g5ednsz&ms=ltu&mt=1577212049&mv=m&mvi=4&pl=19&lsparams=mip,mm,mn,ms,mv,mvi,pl&lsig=AHylml4wRQIgOXQz80MGz5Ok8e_wFjZ_YsRl49Mzl2vSPdBLyUdhIrgCIQCOa6EpKdzW8eYZzEgGChqs-un4POPsfqw6Y1gdEHotgg==',
+              }}
+              style={styles.mediaPlayer}
+              volume={this.state.rendered ? 1 : 0}
+            />
+            <MediaControls
+              duration={this.state.duration}
+              isLoading={this.state.isLoading}
+              mainColor="#333"
+              onFullScreen={this.onFullScreen}
+              onPaused={this.onPaused}
+              onReplay={this.onReplay}
+              onSeek={this.onSeek}
+              onSeeking={this.onSeeking}
+              playerState={this.state.playerState}
+              progress={this.state.currentTime}
+              toolbar={this.renderToolbar()}
+            />
           </View>
-        )}
-      </View>
+          {this.state.width == 150 && this.detailsView()}
+        </View>
+        {this.state.width != 150 && this.detailsView(true)}
+      </>
     );
   }
 }
@@ -168,7 +172,6 @@ const styles = StyleSheet.create({
   },
   description: {
     fontSize: 12,
-    width: 150,
     marginTop: 10,
   },
   sharedby: {
